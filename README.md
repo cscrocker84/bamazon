519b7f20fd75b03667e955200ab66bf6f8f1dc39
Charlie Crocker <cscrocker84@gmail.com>
2019-10-23 14:45:09 -0500

    working on the readme file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# bamazon
+<h1>Bamazon</h1>
+<h3>Bamazon is a Node application that runs accross two different js files</h3>
+<ul>
+<li>bamazonCustomer</li>
+<li>bamazonManager</li>
+</ul>
+<h3>Bamazon Customer View</h3>
+<ul>
+<li>The Bamazon Customer view will allow users to view all of the items for sale.</li>
+<li>The user can then purchase items based on each item's specific Product ID and how many of each item to purchase</li>
+<li>When the user purchases items, the built in calculator provides a total cost for the order</li>
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "Ohiosucks11",
-    database: "bamazon_db"
+    database: "bamazon"
 });
 
 connection.connect(function(err){
@@ -47,4 +47,53 @@ function loadProducts(){
         // run function to allow customer to purchase a product
         buyProducts()
     })
+}
+
+function buyProducts(){
+    // prompt user with id of product they would like to purchase and amount
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the id of the product you would like to purchase?",
+            name: "productId"
+        },
+        {
+            type: "input",
+            messge: "How many would you like to purchase?",
+            name: "purchaseAmount"
+        }
+    ]).then(function(answers){
+        // retrieve stock_quantity by id
+        connection.query("SELECT * FROM products WHERE ?", {id: answers.productId}, function(err, res){
+            if(err) throw err
+            // if stock_quantity is >= answers.purchaseAmount
+            if(res[0].stock_quantity >= parseInt(answers.purchaseAmount)){
+                //  subtract stock_quantity by answers.purchaseAmount
+                var newQuantity = res[0].stock_quantity - parseInt(answers.purchaseAmount)
+                var totalSold = parseInt(answers.purchaseAmount) * res[0].price
+                var currentSales = res[0].product_sales
+                console.log("Price "+res[0].price)
+                console.log("purchase amount "+ parseInt(answers.purchaseAmount))
+                console.log(totalSold)
+                connection.query("UPDATE products SET ? WHERE ?",
+                [
+                        {
+                            stock_quantity: newQuantity,
+                            product_sales: totalSold + currentSales
+                        },
+                        {
+                            id: answers.productId
+                        }
+                ], function(err){
+                    if(err) throw err
+                    console.log("Item purchased")
+                    startBamazon()
+                })
+            } else{
+                // else log error message and run buyProducts()
+                console.log("You can't buy that many")
+                buyProducts()
+            }        
+        })
+    })  
 }
